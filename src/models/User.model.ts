52dca84b66c6506d63f892fc787806d968acc286
gurparvez <gurparvez04@gmail.com>
2025-09-06 13:20:09 +0530

    user model

---
+++ src/models/User.model.ts
@@ -0,0 +1,96 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt, { Secret, SignOptions } from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError";
+
+
+interface IUser extends Document {
+    auid: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    role: "student" | "mentor" | "hod";
+
+    isPasswordCorrect(password:String):Promise<boolean>;
+    accessToken():String;
+}
+
+const userSchema: Schema = new Schema(
+    {
+        auid: {
+            type: String,
+            unique: true,
+            required: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+        lastName: {
+            type: String,
+            required: false,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: false,
+            unique: true
+        },
+        phone: {
+            type: String,
+            required: false,
+            unique: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+)
+
+userSchema.pre<IUser>("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err: any) {
+        next(err);
+    }
+});
+userSchema.methods.accessToken = function (): string {
+  const secret: Secret | undefined = process.env.ACCESS_TOKEN_SECRET;
+  if (!secret) {
+    throw new ApiError(400, "Failed to get secret from env file.");
+  }
+
+  const options: SignOptions = {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY as any,
+  };
+
+  return jwt.sign(
+    {
+      _id: this._id,
+      role: this.role,
+    },
+    secret,
+    options
+  );
+};
+
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export { User };
+export type { IUser };
+
---
+++ types/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    ACCESS_TOKEN_SECRET: string;
+    ACCESS_TOKEN_EXPIRY: string;
+  }
+}
